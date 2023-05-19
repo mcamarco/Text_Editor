924c828936fca9d4ee68ceb7d2040092e794c7e5
Mickey Camarco <48422179+mcamarco@users.noreply.github.com>
2023-05-19 15:38:00 -0400

    completed

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -27,4 +27,15 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+new StaleWhileRevalidate({
+  // Name of the cache storage.
+  cacheName: 'asset-cache',
+  plugins: [
+    // This plugin will cache responses with these headers to a maximum-age of 30 days
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+  ],
+})
+);
